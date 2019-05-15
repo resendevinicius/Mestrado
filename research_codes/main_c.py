3b3eca50f2842da183406d39302ee09980bf902e
resendevinicius <resendeviniciush@gmail.com>
2019-05-15 12:47:19 -0300

    .

--- research_codes/main_c.py
+++ research_codes/main_c.py
@@ -1,4 +1,3 @@
-from sklearn.svm import SVC
 from skmultilearn.dataset import load_dataset_dump
 from skmultilearn.problem_transform import BinaryRelevance
 from skmultilearn.problem_transform import ClassifierChain
@@ -6,15 +5,14 @@ from sklearn import tree
 from skmultilearn.adapt import MLkNN
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
-from sklearn.model_selection import GridSearchCV
 from sklearn import svm
 import ComplexMul as cm
 import numpy as np
 import sklearn.metrics as metrics
 import operator
 
-X_train, y_train, features_train, labels_train = load_dataset_dump('scikit_ml_learn_data/emotions-train.scikitml.bz2')
-X_test, y_test, features_test, labels_test = load_dataset_dump('scikit_ml_learn_data/emotions-test.scikitml.bz2')
+X_train, y_train, features_train, labels_train = load_dataset_dump('scikit_ml_learn_data/scene-train.scikitml.bz2')
+X_test, y_test, features_test, labels_test = load_dataset_dump('scikit_ml_learn_data/scene-test.scikitml.bz2')
 
 X = np.concatenate((X_train.toarray(), X_test.toarray()))
 y = np.concatenate((y_train.toarray(), y_test.toarray()))
@@ -35,7 +33,7 @@ for k in ks:
   for lambd in lambds:
     for threshold in thresholds:
       for classifier in classifiers:
-        parameters_ = 'k: ' + str(k) + ' Lambda: ' + str(lambd) + ' Threshold: ' + str(threshold) + ' Classifier: ' + type(classifier).__name__ + ' ' + str(classifier.classifier).split('(')[0]
+        parameters_ = 'k: ' + str(k) + ' Lambda: ' + str(lambd) + ' Threshold: ' + str(threshold) + ' Classifier: ' + type(classifier).__name__ + '(' + str(classifier.classifier).split('(')[0] + ')'
         scores.update({parameters_: 0})
 
 for train_index, test_index in kf.split(X):
@@ -45,11 +43,15 @@ for train_index, test_index in kf.split(X):
         for classifier in classifiers:
           model = cm.ComplexMul(k = k, classifier = classifier, lambd = lambd, threshold = threshold)
           model.fit(X[train_index], y[train_index])
-          parameters_ = 'k: ' + str(k) + ' Lambda: ' + str(lambd) + ' Threshold: ' + str(threshold) + ' Classifier: ' + type(classifier).__name__ + ' ' + str(classifier.classifier).split('(')[0]
+          parameters_ = 'k: ' + str(k) + ' Lambda: ' + str(lambd) + ' Threshold: ' + str(threshold) + ' Classifier: ' + type(classifier).__name__ + '(' + str(classifier.classifier).split('(')[0] + ')'
           value = (metrics.accuracy_score(model.predict(X[test_index]).flatten(), y[test_index].flatten()))
           scores[parameters_] += value
       
 ans = max(scores.items(), key = operator.itemgetter(1))
 
-print('Avg Score: ' + str(ans[1] / number_splits) + '\n' + str(ans[0]))
+for key, val in scores.items():
+  print(key + '\n' + 'Avg Score: ' + str(val / number_splits))
+
+print('-----------------------------------------------------------')
+print('\nBest Avg Score: ' + str(ans[1] / number_splits) + '\n' + str(ans[0]))
 
